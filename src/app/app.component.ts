73a40111b6f7f62a5997cbeff9a10bad5fe89c9f
JhonnysDS <98484070+JhonnysDS@users.noreply.github.com>
2022-10-12 17:39:20 -0500

    agregar hecho, editar en proceso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { provideImageKitLoader } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { Employee } from './models/employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-simple-crud';
+  employeeArray: Employee[] = [
+    {
+      id: 1,
+      name: "Kalel",
+      country: "kripton"
+    },
+    {
+      id: 2,
+      name: "peter parker",
+      country: "USA"
+    },
+    {
+      id: 3,
+      name: "Bruce Wayne",
+      country: "Gotham"
+    }
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit(){
+    this.selectedEmployee.id = this.employeeArray.length + 1;
+    this.employeeArray.push(this.selectedEmployee);
+
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+  
 }
